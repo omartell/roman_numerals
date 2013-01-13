9e54f16959589439ac7deb5bb8ea1406d473536f
Oliver Martell <oliver.martell@gmail.com>
2013-01-13 12:26:03 +0000

    Add some other basic test cases

--- lib/roman_numerals/roman_numerals.rb
+++ lib/roman_numerals/roman_numerals.rb
@@ -2,8 +2,24 @@ module RomanNumerals
   def self.translate(normal)
     if normal == 1
       "I"
-    else
+    elsif normal == 2
       "II"
+    elsif normal == 3
+      "III"
+    elsif normal == 4
+      "IV"
+    elsif normal == 5
+      "V"
+    elsif normal == 6
+      "VI"
+    elsif normal == 7
+      "VII"
+    elsif normal == 8
+      "VIII"
+    elsif normal == 9
+      "IX"
+    else
+      "X"
     end
   end
 end
--- spec/roman_numerals/roman_numerals_spec.rb
+++ spec/roman_numerals/roman_numerals_spec.rb
@@ -5,9 +5,32 @@ module RomanNumerals
       it "handles 1" do
         subject.translate(1).should eq "I"
       end
-
       it "handles 2" do
         subject.translate(2).should eq "II"
+      end
+      it "handles 3" do
+        subject.translate(3).should eq "III"
+      end
+      it "handles 4" do
+        subject.translate(4).should eq "IV"
+      end
+      it "handles 5" do
+        subject.translate(5).should eq "V"
+      end
+      it "handles 6" do
+        subject.translate(6).should eq "VI"
+      end
+      it "handles 7" do
+        subject.translate(7).should eq "VII"
+      end
+      it "handles 8" do
+        subject.translate(8).should eq "VIII"
+      end
+      it "handles 9" do
+        subject.translate(9).should eq "IX"
+      end
+      it "handles 10" do
+        subject.translate(10).should eq "X"
       end
     end
   end
