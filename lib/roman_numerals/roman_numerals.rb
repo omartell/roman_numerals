d1bf75d1376947e20052004544b89f7ae3e72aa1
Oliver Martell <oliver.martell@gmail.com>
2013-01-14 22:26:25 +0000

    The obvious implementation

--- lib/roman_numerals/roman_numerals.rb
+++ lib/roman_numerals/roman_numerals.rb
@@ -26,6 +26,16 @@ module RomanNumerals
       end
     end
 
+    if ((10 - number) == 1) || ((number % 10) >= 0 && number >= 10)
+      if (10 - number) == 1
+        result += UNITS["1"] + UNITS["10"]
+        number = 0
+      else
+        result += UNITS["10"]
+        number = number - 10
+      end
+    end
+
     if ((5 - number) == 1) || ((number % 5) >= 0 && number >= 5)
       if (5 - number) == 1
         result += UNITS["1"] + UNITS["5"]
